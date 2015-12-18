22d9f303335af46a1e63796d1e8d13c3efa0d23b
helpfulrobot <chris@silverstripe.com>
2015-12-18 14:10:47 +1300

    Converted to PSR-2

--- code/pages/GlobalErrorPage.php
+++ code/pages/GlobalErrorPage.php
@@ -1,15 +1,17 @@
 <?php
 
-class GlobalErrorPage extends ErrorPage {
+class GlobalErrorPage extends ErrorPage
+{
 
-	private static $description = 'Default to a sinlge error page';
+    private static $description = 'Default to a sinlge error page';
 
     /**
      * Stop the static html file being created in assets
      */
-	public function doPublish() {
-		return true;
-	}
+    public function doPublish()
+    {
+        return true;
+    }
 }
 
 /**
@@ -17,49 +19,52 @@ class GlobalErrorPage extends ErrorPage {
  *
  * @package cms
  */
-class GlobalErrorPage_Controller extends ErrorPage_Controller {
+class GlobalErrorPage_Controller extends ErrorPage_Controller
+{
 
-	private $defaultErrorPagePath = '/var/www/error_pages/';
+    private $defaultErrorPagePath = '/var/www/error_pages/';
 
-	public function handleRequest(SS_HTTPRequest $request, DataModel $model = NULL) {
-		$body = null;
-		$lang = i18n::get_locale();
-        	$path = Config::inst()->get('GlobalErrorPage', 'GlobalErrorPagePath');
-		if (!$path) $path = $this->defaultErrorPagePath;
-		$errorPages = array(
-        		Config::inst()->get('GlobalErrorPage', $this->ErrorCode),
-			$path . "error-{$this->ErrorCode}-$lang.html",
-			$path . "error-{$this->ErrorCode}-$lang.php",
-			$path . 'error.html',
-			$path . 'error.php'
-		);
-		$this->extend('updateHandleRequest', $errorPages);
-		// now check if any of the pages exist
-		foreach ($errorPages as $errorPage) {
-			if (!$body && file_exists($errorPage)) {
-				$ext = pathinfo($errorPage, PATHINFO_EXTENSION);
-				if ($ext == 'php') {
-					ob_start();
-					include $errorPage;
-					$body = ob_get_clean();
-				} else {
-					$body = file_get_contents($errorPage);
-				}
-				break;
-			}
-		}
+    public function handleRequest(SS_HTTPRequest $request, DataModel $model = null)
+    {
+        $body = null;
+        $lang = i18n::get_locale();
+        $path = Config::inst()->get('GlobalErrorPage', 'GlobalErrorPagePath');
+        if (!$path) {
+            $path = $this->defaultErrorPagePath;
+        }
+        $errorPages = array(
+                Config::inst()->get('GlobalErrorPage', $this->ErrorCode),
+            $path . "error-{$this->ErrorCode}-$lang.html",
+            $path . "error-{$this->ErrorCode}-$lang.php",
+            $path . 'error.html',
+            $path . 'error.php'
+        );
+        $this->extend('updateHandleRequest', $errorPages);
+        // now check if any of the pages exist
+        foreach ($errorPages as $errorPage) {
+            if (!$body && file_exists($errorPage)) {
+                $ext = pathinfo($errorPage, PATHINFO_EXTENSION);
+                if ($ext == 'php') {
+                    ob_start();
+                    include $errorPage;
+                    $body = ob_get_clean();
+                } else {
+                    $body = file_get_contents($errorPage);
+                }
+                break;
+            }
+        }
 
-		if ($body) {
-			$response = new SS_HTTPResponse();	
-			$response->setStatusCode($this->ErrorCode);
-			$response->setBody($body);
-			return $response;
-		}
+        if ($body) {
+            $response = new SS_HTTPResponse();
+            $response->setStatusCode($this->ErrorCode);
+            $response->setBody($body);
+            return $response;
+        }
 
-		$body = parent::handleRequest($request, $model);
-		$this->response->setStatusCode($this->ErrorCode);
+        $body = parent::handleRequest($request, $model);
+        $this->response->setStatusCode($this->ErrorCode);
 
-		return $this->response;
-	}
+        return $this->response;
+    }
 }
-
